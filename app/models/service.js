5c156fb2f90ca7d7b0af0144564dd5536fec3b13
Ariel <arielcv96@gmail.com>
2019-07-02 11:14:35 -0400

    #1 - Translate service model

--- app/models/service.js
+++ app/models/service.js
@@ -4,11 +4,26 @@
 
 'use strict';
 
-var mongoose = require('mongoose');
+const fieldSchema = {
+  name: 's',
+};
+
+const tagSchema = {
+};
+
+const name = 'Service';
+
+module.exports = function(influx) {
+  influx.schema(name, fieldSchema, tagSchema, {
+    stripUnknown: true,
+  });
+};
+
+/*var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
 var ServiceSchema = new Schema({
   name  : { type: String, required: true },
 });
 
-mongoose.model('Service', ServiceSchema);
+mongoose.model('Service', ServiceSchema);*/
